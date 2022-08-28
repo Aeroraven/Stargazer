07902971240a783eb9333661c5f315afa0676592
Mizuki_1950641 <70007256+Aeroraven@users.noreply.github.com>
2022-08-28 23:44:17 +0800

    TR: Normal Mapping

--- ComputerGraphics/Aria/C4S2-Normal-Mapping/dist/african-fragment.glsl
+++ ComputerGraphics/Aria/C4S2-Normal-Mapping/dist/african-fragment.glsl
@@ -3,26 +3,15 @@ varying mediump vec4 vColor;
 varying mediump vec2 vTex;
 varying mediump vec3 vNorm;
 varying mediump vec3 vFragPos;
-varying mediump vec3 vTangent;
-varying mediump vec3 vBitangent;
 
 uniform sampler2D uSampler;
 uniform sampler2D uSpecSampler;
-uniform sampler2D uNormSampler;
-uniform mat4 uModel;
 uniform vec3 uCamPos;
 
 void main(){
     vec4 lightCol = vec4(1.0);
     vec4 texCol = vColor * 0.05 + texture2D(uSampler,vec2(vTex.s,vTex.t)) * 0.95 ;
     vec3 lightPos = vec3(1.5,0.5,2.0);
-    lightPos = (uModel * vec4(lightPos,1.0)).xyz;
-    
-    //Normal mapping
-    vec3 normTex = texture2D(uNormSampler,vec2(vTex.s,vTex.t)).xyz;
-    normTex = normalize(normTex*2.0-1.0);
-    mat3 tbn = mat3(normalize(vTangent),normalize(vBitangent),normalize(vNorm));
-    vec3 nNorm = tbn*normTex;
 
     //Ambient
     float ambientStrength = 0.1;
@@ -30,7 +19,7 @@ void main(){
 
     //Diffuse
     float diffuseStrength = 0.4;
-    vec3 norm = normalize(nNorm);
+    vec3 norm = normalize(vNorm);
     vec3 lightDir = normalize(lightPos-vFragPos);
     float diffusePower = dot(norm,lightDir);
     vec4 diffusePart = lightCol * diffuseStrength * diffusePower;
@@ -43,6 +32,6 @@ void main(){
     vec3 specTex = texture2D(uSpecSampler,vec2(vTex.s,vTex.t)).xyz;
     vec4 specularPart = specStrength * spec * lightCol * vec4(specTex.xyz,1.0);
 
-    //gl_FragColor = vec4(nNorm.xyz,1.0);
+    //gl_FragColor = vec4(1,0,0,1);
     gl_FragColor = (ambientPart + diffusePart + specularPart)*texCol;
 }
---
+++ ComputerGraphics/Aria/C4S2-Normal-Mapping/src/index.ts
@@ -0,0 +1,150 @@
+
+import { mat4,mat3 } from "../node_modules/gl-matrix-ts/dist/index";
+import { AriaCamera } from "./core/aria-camera"
+import { AriaFramebuffer } from "./core/aria-framebuffer";
+import {initShaderProgram, loadFile, loadImage,createTexture } from "./core/aria-base"
+import { AriaShader } from "./core/aria-shader";
+import { AriaBufferMap } from "./core/aria-buffer-map";
+import { AriaTextureMap } from "./core/aria-texture-map";
+import { AriaComRectangle } from "./components/aria-com-rectangle";
+import { AriaComAfrican } from "./components/aria-com-african";
+import { AriaComSimLightBox } from "./components/aria-com-sim-light-box";
+import { AriaComSkybox } from "./components/aria-com-skybox";
+import { AriaComAfricanInstancing } from "./components/aria-com-african-instancing";
+import { AriaComFloor } from "./components/aria-com-floor";
+
+function clearScene(gl:WebGL2RenderingContext){ 
+    //Clear
+    gl.clearColor(0,0,0,1);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
+} 
+
+
+async function loadTex(gl:WebGL2RenderingContext):Promise<AriaTextureMap>{
+    const texImg = await loadImage("./african_diffuse.jpg")
+    const tex = createTexture(gl,texImg);
+    const specTexImg = await loadImage("./african_specular.jpg")
+    const specTex = createTexture(gl,specTexImg)
+    const postTexImg = await loadImage("./star.jpg")
+    const postTex = createTexture(gl,postTexImg)
+    const floorTexImg = await loadImage("./floor.jpg")
+    const floorTex = createTexture(gl,floorTexImg)
+    const normTexImg = await loadImage("./normal.jpg")
+    const normTex = createTexture(gl,normTexImg)
+
+
+    const r = new AriaTextureMap()
+    r.set("tex1",tex)
+    r.set("tex2",specTex)
+    r.set("tex3",postTex)
+    r.set("texFloor",floorTex)
+    r.set("texNorm",normTex)
+    return r
+}
+
+async function main(){
+    const camera = new AriaCamera()
+    const canvas = <HTMLCanvasElement>(document.getElementById("webgl_displayer"));
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    const gl = <WebGL2RenderingContext>canvas.getContext("webgl2", { stencil: true });
+
+    gl.viewport(0,0,window.innerWidth,window.innerHeight)
+    gl.disable(gl.CULL_FACE)
+
+    //Stencil Test
+    gl.enable(gl.STENCIL_TEST)
+    gl.stencilOp(gl.KEEP,gl.KEEP,gl.REPLACE)
+    gl.stencilFunc(gl.ALWAYS,1,0xff)
+    gl.stencilMask(0xff)
+    //Mesh
+    const african = await loadFile("./african.obj")
+
+    //Camera
+    camera.registerInteractionEvent()
+
+    //Texture
+    
+    //Shader
+    const vSrc = await loadFile("./african-vertex.glsl");
+    const fSrc = await loadFile("./african-fragment.glsl");
+    const africanShader = new AriaShader(gl,vSrc,fSrc);
+    const africanBuffer = AriaComAfrican.initBuffer(gl,african);
+
+    //African Refl Shader
+    const varsrc = await loadFile("./african-skybox-refl-vertex.glsl")
+    const farsrc = await loadFile("./african-skybox-refl-fragment.glsl")
+    const africanReflShader = new AriaShader(gl,varsrc,farsrc)
+
+    //African Refl Instancing Shader
+    const varisrc = await loadFile("./african-skybox-refl-inst-vertex.glsl")
+    const farisrc = await loadFile("./african-skybox-refl-fragment.glsl")
+    const africanReflInstShader = new AriaShader(gl,varisrc,farisrc)
+
+    //Light Shader
+    const vlsrc = await loadFile("./light-vertex.glsl");
+    const flsrc = await loadFile("./light-fragment.glsl");
+    const lightShaderProg = new AriaShader(gl,vlsrc,flsrc);
+    const lightbuf = AriaComSimLightBox.initBuffer(gl)
+
+    //Outline Shader
+    const vosrc = await loadFile("./african-outline-vertex.glsl");
+    const fosrc = await loadFile("./african-outline-fragment.glsl");
+    const outlineShaderProg = new AriaShader(gl,vosrc,fosrc);
+
+    //Post Shader
+    const vpsrc = await loadFile("./post-vertex.glsl")
+    const fpsrc = await loadFile("./post-fragment.glsl")
+    const postShader = new AriaShader(gl,vpsrc,fpsrc)
+    const postbuf = AriaComRectangle.initBuffer(gl)
+
+
+    //Skybox Shader
+    const vsksrc = await loadFile("./skybox-vertex.glsl")
+    const fsksrc = await loadFile("./skybox-fragment.glsl")
+    const skyShader = new AriaShader(gl,vsksrc,fsksrc)
+    const skyCom = new AriaComSkybox(gl)
+
+    await skyCom.loadTexFromFolder("./skybox/")
+    skyCom.initBuffer()
+
+    //Floor Shader
+    const vflsrc = await loadFile("./floor-vertex.glsl")
+    const fflsrc = await loadFile("./floor-fragment.glsl")
+    const floorShader = new AriaShader(gl,vflsrc,fflsrc)
+    const floorbuf = AriaComFloor.initBuffer(gl)
+
+    //Normal Mapping Shader
+    const vnmsrc = await loadFile("./african-tangent-vertex.glsl")
+    const fnmsrc = await loadFile("./african-tangent-fragment.glsl")
+    const normalShader = new AriaShader(gl,vnmsrc,fnmsrc)
+
+    //Texture
+    const texStruct = await loadTex(gl)
+
+    //FrameBuffer
+    const framebuf = new AriaFramebuffer(gl)
+    texStruct.set("tex3",framebuf.tex)
+    texStruct.set("texSky",skyCom.cubeTex)
+
+    //Render
+    function render(){
+        //First pass
+        framebuf.bind()
+        clearScene(gl)
+        //skyCom.draw(skyShader,camera)
+        AriaComAfrican.draw(gl,normalShader,africanBuffer,texStruct,camera)
+        //AriaComFloor.draw(gl,floorShader,floorbuf,texStruct,camera)
+        framebuf.unbind()
+
+        //Postprocessing
+        clearScene(gl)
+        AriaComRectangle.draw(gl,postShader,postbuf,texStruct,camera)
+        requestAnimationFrame(render)
+    }
+    requestAnimationFrame(render)
+}
+
+main()
